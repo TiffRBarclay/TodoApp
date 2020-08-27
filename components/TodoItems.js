a3918c32d6ec85e5e0877657388aea361821d865
Tiffany Barclay <tiffany@barent.co.nz>
2020-08-27 14:19:33 +1200

    Add delete icon to items

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 const TodoItems = ({ item, handleItemClick }) => {
   return (
     <TouchableOpacity onPress={() => handleItemClick(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <AntDesign name="delete" size={20} color="black" />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  text: {
+    marginLeft: 10,
   },
 });
 
